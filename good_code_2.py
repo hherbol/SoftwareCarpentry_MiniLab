395c6085dd1efed527899c4e891cb6bc4490ccf7
Prabhjot Kaur <pluthra2@jhu.edu>
2019-09-10 03:07:15 +0530

    Made changes and fixed the bugs!

---
+++ good_code_2.py
@@ -0,0 +1,4 @@
+if __name__ == "__main__":
+    # For your own health, I am cutting you off to a max of 4 beers.
+    for i in range(4, 0, -1):
+        print("%d bottles of beer on the wall, %d bottles of beer.  Take one down, pass it around, %d bottles of beer on the wall." % (i, i, (i-1)))
