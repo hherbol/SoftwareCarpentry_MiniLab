1414e0c09c1c36aa6bcf2b6cc734a057a841d72e
Casey Zhu <czhu21@jhu.edu>
2019-09-09 17:22:02 -0400

    Fixed versions of the bad_code_1 and bad_code_2 files

--- bad_code_1.py
+++ bad_code_1.py
@@ -1,3 +1,6 @@
+#Casey Zhu 
+#bad_code_1.py edit
+
 if __name__ == "__main__":
-print 'I hate it when people call me "Fifa", that's my cat's name!'
+    print('I hate it when people call me "Fifa", that\'s my cat\'s name!')
 
